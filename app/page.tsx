44101ad5191023626e7ba226f863ba5dd9b97db3
matteoilling <matteo.illing@icloud.com>
2023-11-01 21:57:30 +0100

    Updated font import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from "@/app/ui/acme-logo";
-import { lusitana } from "./ui/font";
+import { lusitana } from "./ui/fonts";
 import Image from "next/image";
 
 import Link from "next/link";
